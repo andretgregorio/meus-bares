6b3c62664c0b29e2c55b0117cfba58e7fed01b27
André T Gregório <andretgregorio@gmail.com>
2023-02-05 17:28:34 -0300

    :lipstick: Add container div

--- app/places/views/pages/admin.tsx
+++ app/places/views/pages/admin.tsx
@@ -1,7 +1,9 @@
 export default function AdminPage() {
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <p>Hello, admin</p>
+      <div className="m-auto w-full max-w-4xl">
+        <p>Hello, admin</p>
+      </div>
     </main>
   );
 }
